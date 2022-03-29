74830e8901f1926b81a026bc6a7b028bd317b2ba
jsvillamayor <78101377+jsvillamayor@users.noreply.github.com>
2022-03-29 12:17:00 -0700

    Add files via upload

---
+++ Variables/src/Main.java
@@ -0,0 +1,20 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int x = 123; //initialization
+		double y = 3.14;
+		boolean z = true;
+		char symbol = '@';
+		String name  = "James";
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+		System.out.println(symbol);
+		System.out.println(name);
+
+	}
+
+}
